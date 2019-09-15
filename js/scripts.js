c230fa480fda0612a54f73285a626d56f24f238a
Jess Leonard <jesspaxton23@gmail.com>
2019-09-15 14:43:44 -0500

    Add multiply, divide and subtract function and CSS styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,54 @@
+
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
-
-var subtract = function(number3, number4) {
-  return number3 - number4;
+var subtract = function(number1, number2) {
+  return number1 - number2;
 };
 
-var number3 = parseInt(prompt("Enter a number:"));
-var number4 = parseInt(prompt("Enter another number:"));
-var result2 = subtract(number3, number4);
-alert(result2);
-
-var multiply = function(number5, number6) {
-  return number5 * number6;
+var multiply = function(number1, number2) {
+  return number1 * number2;
 };
 
-var number5 = parseInt(prompt("Enter a number:"));
-var number6 = parseInt(prompt("Enter another number:"));
-var result3 = multiply(number5, number6);
-alert(result3);
-
-var divide = function(number7, number8) {
-  return number7 / number8;
+var divide = function(number1, number2) {
+  return number1 / number2;
 };
 
-var number7 = parseInt(prompt("Enter a number:"));
-var number8 = parseInt(prompt("Enter another number:"));
-var result4 = divide(number7, number8);
-alert(result4);
+// Everything below this line is user interface (or front-end) logic:
 
-function cToF(celsius) 
-{
-  var cTemp = parseInt(prompt("Enter a whole celsius temperature:"));
-  var cToFahr = cTemp * 9 / 5 + 32;
-  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
-    console.log(message);
-}
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output1").text(result);
+  });
 
-function fToC(fahrenheit) 
-{
-  var fTemp = fahrenheit;
-  var fToCel = (fTemp - 32) * 5 / 9;
-  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
-    console.log(message);
-} // From an online resource - change to calculator way from before
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
 
-var convertToF = function(celsius) {
-	return cTemp * 9 / 5 + 32;
-};
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
 
-var cTemp = parseInt(prompt("Enter a whole celsius temperature:"));
-var result = "That is " + (cTemp * 9 / 5 + 32) + " degrees Fahrenheit.";
-alert(result);
-
-var convertToC = function(fahrenheit) {
-	return (fTemp - 32) * 5/9;
-};
-
-var fTemp = parseInt(prompt("Enter a whole fahrenheit temperature:"));
-var result2 = "That is " + (fTemp - 32) * 5/9 + " degrees Celsius.";
-alert(result2);
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output4").text(result);
+  });
+});
